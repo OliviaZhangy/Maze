84ee4767c71f0ffb18f11ad422e27efdcbda6f0c
Mirage09 <borismirage00@gmail.com>
2018-07-27 11:24:34 -0700

    Add Explanation

--- src/MazeCoord.java
+++ src/MazeCoord.java
@@ -1,4 +1,6 @@
 /**
+ * This class define some maze property.
+ *
  * @author BorisMirage
  * Time: 2018/07/26 20:51
  * Created with IntelliJ IDEA
@@ -9,19 +11,40 @@ public class MazeCoord {
     final private int row;
     final private int col;
 
+    /**
+     * Pass para into class
+     *
+     * @param row row number (start from 0)
+     * @param col column number (start from 0)
+     */
     public MazeCoord(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
+    /**
+     * Get current coord's row number.
+     *
+     * @return int row number
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Get current coord's column number.
+     *
+     * @return int column number
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * 
+     * @param other
+     * @return
+     */
     public boolean equals(Object other) {
 
         if (other == null) {
